27d481287c04ae1ec956a4f31bcdda869a5b7cf3
rhd9228 <rhd9228@gmail.com>
2023-07-27 20:43:29 +0900

    첫첫커밋

---
+++ elevenProject/CartPage/cartClass.swift
@@ -0,0 +1,50 @@
+
+import Foundation
+
+
+class Cart { // 장바구니
+    
+    var cartList:[SubwaySandwitch] = [
+        EggMayo("Eggmayo",6700),
+        ItalianBMT("ItalianBMT",0),
+        Tuna("Tuna",0),
+        MeatBall("MeatBall",0),
+        Turkey("Turkey",0),
+        Ham("Ham",0)
+    ] //임시방편임
+    
+    func addMenu(_ sandwitch:SubwaySandwitch) { // 장바구니 추가
+        // 나중에 구매 페이지에서 실행될 메소드
+        cartList.append(sandwitch)
+    }
+    func deleteMenu(index:Int) { // 장바구니 지우기
+        self.cartList.remove(at: index)
+    }
+    
+    func cartListPrint(_ text:String){ // 카트 리스트 프린트해주는 함수
+        if myCart.cartList.isEmpty == true {
+            print("장바구니에 아무것도 없습니다.")
+        }else{
+            var myCartNameList:[String] = []
+            for i in myCart.cartList{
+                myCartNameList.append(i.name)
+            }
+            startHomeMenu(title: "장바구니 \(text)", menu: myCartNameList)
+        }
+    }
+    
+    func cartListInfo(){
+        cartListPrint("조회")
+        readLine()
+    }
+    func cartListDelete(){
+        cartListPrint("삭제")
+        let input:Int = Int(readLine() ?? "-1") ?? -1
+        if (input < 0) || (input > myCart.cartList.count) {
+            print("잘못입력됨")
+        }else{
+            myCart.deleteMenu(index: input-1)
+            print("삭제되었습니다.")
+        }
+    }
+}
